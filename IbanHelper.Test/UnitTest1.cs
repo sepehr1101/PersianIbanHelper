6d65266c2244940b20ae5ec637c61f3792d68d68
_1101 <mantera.sh@gmail.com>
2017-08-25 18:16:38 +0430

    initial commit

---
+++ IbanHelper.Test/UnitTest1.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using IbanHelper;
+
+namespace IbanHelper.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        #region not match regex
+        [TestMethod]
+        public void TestInvalidRegex1()
+        {
+            var expectedResult = false;
+            string iban = "IX062960000000100324200001";//IX
+            var validationResult = IbanUtil.ValidateIBAN(iban);
+            Assert.AreEqual(expectedResult, validationResult);
+        }
+        [TestMethod]
+        public void TestInvalidRegex2()
+        {
+            var expectedResult = false;
+            string iban = "IR0629600000001003242000013";//27 char
+            var validationResult = IbanUtil.ValidateIBAN(iban);
+            Assert.AreEqual(expectedResult, validationResult);
+        }
+        [TestMethod]
+        public void TestInvalidRegex3()
+        {
+            var expectedResult = false;
+            string iban = "IRR62960000000100324200001";//IRR
+            var validationResult = IbanUtil.ValidateIBAN(iban);
+            Assert.AreEqual(expectedResult, validationResult);
+        }
+        [TestMethod]
+        public void TestInvalidRegex4()
+        {
+            var expectedResult = false;
+            string iban = "IR6296000000010032420000";//25 char
+            var validationResult = IbanUtil.ValidateIBAN(iban);
+            Assert.AreEqual(expectedResult, validationResult);
+        }
+        #endregion
+
+        #region mellat
+        [TestMethod]
+        public void TestTrueMellat1()
+        {
+            var expectedResult = true;
+            string iban = "IR062960000000100324200001";
+            var validationResult = IbanUtil.ValidateIBAN(iban);
+            Assert.AreEqual(expectedResult, validationResult);
+        }
+        [TestMethod]
+        public void TestFalseMellat1()
+        {
+            var expectedResult = false;
+            string iban = "IR062960000000100324200002";
+            var validationResult = IbanUtil.ValidateIBAN(iban);
+            Assert.AreEqual(expectedResult, validationResult);
+        }
+        [TestMethod]
+        public void TestTrueMellat2()
+        {
+            var expectedResult = true;
+            string iban = "IR500120000000000266719902";
+            var validationResult = IbanUtil.ValidateIBAN(iban);
+            Assert.AreEqual(expectedResult, validationResult);
+        }
+       
+       [TestMethod]
+        public void TestFalseMellat2()
+        {
+            var expectedResult = false;
+            string iban = "IR500120000000000266719903";
+            var validationResult = IbanUtil.ValidateIBAN(iban);
+            Assert.AreEqual(expectedResult, validationResult);
+        }
+        #endregion
+
+        #region melli
+        [TestMethod]
+        public void TestTrueMelli1()
+        {
+            var expectedResult = true;
+            string iban = " IR380170000000104183555002";
+            var validationResult = IbanUtil.ValidateIBAN(iban);
+            Assert.AreEqual(expectedResult, validationResult);
+        }
+        [TestMethod]
+        public void TestFalseMelli1()
+        {
+            var expectedResult = false;
+            string iban = " IR380170000000104183555004";
+            var validationResult = IbanUtil.ValidateIBAN(iban);
+            Assert.AreEqual(expectedResult, validationResult);
+        }
+        #endregion
+
+        #region saderat
+        [TestMethod]
+        public void TestTrueSaderat1()
+        {
+            var expectedResult = true;
+            string iban = " IR920190000000110017470004";
+            var validationResult = IbanUtil.ValidateIBAN(iban);
+            Assert.AreEqual(expectedResult, validationResult);
+        }
+        [TestMethod]
+        public void TestFalseSaderat1()
+        {
+            var expectedResult = false;
+            string iban = " IR920190000000110017470008";
+            var validationResult = IbanUtil.ValidateIBAN(iban);
+            Assert.AreEqual(expectedResult, validationResult);
+        }
+        #endregion
+
+        #region parsian
+        [TestMethod]
+        public void TestTrueParsian()
+        {
+            var expectedResult = true;
+            string iban = "IR062960000000100324200001";
+            var validationResult = IbanUtil.ValidateIBAN(iban);
+            Assert.AreEqual(expectedResult, validationResult);
+        }
+        [TestMethod]
+        public void TestFalseParsian()
+        {
+            var expectedResult = false;
+            string iban = "IR062760000000100324200001";
+            var validationResult = IbanUtil.ValidateIBAN(iban);
+            Assert.AreEqual(expectedResult, validationResult);
+        }
+        #endregion
+
+        #region shahr
+        [TestMethod]
+        public void TestTrueShahr()
+        {
+            var expectedResult = true;
+            string iban = "IR390610000000100801920157";
+            var validationResult = IbanUtil.ValidateIBAN(iban);
+            Assert.AreEqual(expectedResult, validationResult);
+        }
+        [TestMethod]
+        public void TestFalseShahr()
+        {
+            var expectedResult = false;
+            string iban = "IR390610000000100801920158";
+            var validationResult = IbanUtil.ValidateIBAN(iban);
+            Assert.AreEqual(expectedResult, validationResult);
+        }
+
+        #endregion
+    }
+}
---
+++ IbanHelper/IbanUtil.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace IbanHelper
+{
+    public static class IbanUtil
+    {
+        private static string GetRegexPattern(bool limitToIran = false)
+        {
+            return limitToIran ? @"^IR([0-9]\s?){24}$" : @"^[A-Z0-9]";
+        }
+        //
+        public static bool ValidateIBAN(string iban, bool limitToIran = true)
+        {
+            var regexPattern = GetRegexPattern(limitToIran);           
+            if (String.IsNullOrWhiteSpace(iban))
+                return false;
+            iban = iban.ToUpper();
+            iban = iban.Replace(" ", String.Empty);
+            if (!Regex.IsMatch(iban, regexPattern))
+            {
+                return false;
+            }    
+            StringBuilder bankCode = GetBankCode(iban);
+            string checkSumString = bankCode.ToString();
+            int checksum = int.Parse(checkSumString.Substring(0, 1));
+            for (int i = 1; i < checkSumString.Length; i++)
+            {
+                int v = int.Parse(checkSumString.Substring(i, 1));
+                checksum *= 10;
+                checksum += v;
+                checksum %= 97;
+            }
+            return checksum == 1;
+        }
+
+        private static StringBuilder GetBankCode(string iban)
+        {
+            const int ASCII_SHIFT = 55;
+            const int FOUR = 4;
+            string bank = iban.Substring(FOUR, iban.Length - FOUR) + iban.Substring(0, FOUR);
+            StringBuilder stringBuilderBank = new StringBuilder();
+            foreach (char c in bank)
+            {
+                int value;
+                if (Char.IsLetter(c))
+                {
+                    value = c - ASCII_SHIFT;
+                }
+                else
+                {
+                    value = int.Parse(c.ToString());
+                }
+                stringBuilderBank.Append(value);
+            }
+            return stringBuilderBank;
+        }     
+    }
+}
